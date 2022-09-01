149aacfd1760814d25bc4646b6cdae598309dfef
kevan Jahanshahi <kevan@jahia.com>
2022-09-01 17:36:53 +0200

    UNOMI-647: base project (#1)
    * UNOMI-647: base folder structure and packaging config for unomi-tracker
    
    * UNOMI-647: add linter and on-code-change workflow

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
+
+export default [
+    {
+        input: "src/index.js",
+        output: {
+            name: pkg.name,
+            file: pkg.browser,
+            format: "umd",
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+                babelHelpers: 'runtime',
+                exclude: ["node_modules/**"],
+            }),
+        ],
+    },
+    {
+        input: "src/index.js",
+        output: [
+            { file: pkg.main, format: "cjs" },
+            { file: pkg.module, format: "es" },
+        ],
+        plugins: [
+            babel({
+                babelHelpers: 'runtime',
+                exclude: ["node_modules/**"],
+            }),
+        ],
+        external: id => id.includes('@babel/runtime')
+    },
+];
