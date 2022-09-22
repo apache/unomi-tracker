2583f7f2a4c32412874760a7fbcc6e632b4e5b7d
kevan Jahanshahi <kevan@jahia.com>
2022-09-22 13:54:55 +0200

    UNOMI-671: add missing stuff for release process (#8)
    * UNOMI-671: add missing stuff for release process
    
    * UNOMI-671: add missing stuff for release process

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import license from 'rollup-plugin-license';
 import pkg from './package.json';
+import * as path from 'path';
 
 export default [
     {
@@ -12,6 +31,15 @@ export default [
             format: 'umd',
         },
         plugins: [
+            license({
+                banner: {
+                    commentStyle: 'regular',
+                    content: {
+                        // eslint-disable-next-line no-undef
+                        file: path.join(__dirname, 'LICENSE_HEADER')
+                    }
+                }
+            }),
             resolve(),
             commonjs(),
             babel({
@@ -27,11 +55,20 @@ export default [
             { file: pkg.module, format: 'es' },
         ],
         plugins: [
+            license({
+                banner: {
+                    commentStyle: 'regular',
+                    content: {
+                        // eslint-disable-next-line no-undef
+                        file: path.join(__dirname, 'LICENSE_HEADER')
+                    }
+                }
+            }),
             babel({
                 babelHelpers: 'runtime',
                 exclude: ['node_modules/**'],
             }),
         ],
-        external: id => id.includes('@babel/runtime')
+        external: id => id === 'es6-crawler-detect' || id.includes('@babel/runtime')
     },
 ];
