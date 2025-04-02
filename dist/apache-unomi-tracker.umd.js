c2718079d2499113876f281c34039a46743c36a3
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2025-04-02 15:35:23 +0200

    feat: produce and expose TypeScript types (#21)

--- dist/apache-unomi-tracker.umd.js
+++ dist/apache-unomi-tracker.umd.js
@@ -1678,7 +1678,7 @@
   function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
   function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-  var newTracker = function newTracker() {
+  var useTracker = function useTracker() {
     var wem = {
       /**
        * This function initialize the tracker
@@ -3243,26 +3243,6 @@
       }
     };
     return wem;
-  };
-
-  /*
-   * Licensed to the Apache Software Foundation (ASF) under one or more
-   * contributor license agreements.  See the NOTICE file distributed with
-   * this work for additional information regarding copyright ownership.
-   * The ASF licenses this file to You under the Apache License, Version 2.0
-   * (the "License"); you may not use this file except in compliance with
-   * the License.  You may obtain a copy of the License at
-   *
-   *      http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
-  var useTracker = function useTracker() {
-    return newTracker();
   };
 
   exports.useTracker = useTracker;
